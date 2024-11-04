b0b98a70594675a580c9da3056b6c47e55db6ac2
Luis Alberto Oliveira <63371167+LuisAlbertoOliveira@users.noreply.github.com>
2024-11-03 23:48:10 -0300

    Alterado o nome do arquivo de teste com a amazon

--- cypress/e2e/amazon.cy.js
+++ cypress/e2e/amazon.cy.js
@@ -1,15 +1,15 @@
 describe('Teste automatizado', () => {
-    it('Visitando o google ', () => {
+    it('Visitando a Amazon ', () => {
       cy.visit('https://www.amazon.com.br/')
-     // cy.visit('https://example.cypress.io')
-
+     
       cy.contains('Contas e Listas').click()
 
       cy.url().should('include','/ap/signin')
 
-      // Get an input, type into it
+      // Para pegar o nome da classe, deve-se inspecionar a página que se deseja testar e verificar a classe do elemento. O comando cy.get trabalha com o nome da classe.
     cy.get('.a-form-label').type('oliv.luis@gmail.com')
 
+    // Foi difícil achar o nome da classe desse botão tive que fazer vários testes. 
     cy.get('#continue.a-button-input').click()
 
     })
